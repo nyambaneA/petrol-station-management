e4511e57d96117629597bae1f7913c37064704d2
NyambaneA <nyambanealfred1539@gmail.com>
2025-03-11 17:56:32 +0300

    added static cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -16,21 +16,20 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000", "https://fueltrack-oigw.onrender.com/"];
 
-const allowedOrigins = 'http://localhost:3000'
-app.use(cors({
-    origin: function(origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        return callback(null, true);
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
       } else {
-        return callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
-    }
-  }));
-
+    },
+    credentials: true,
+  })
+);
 // app.use(cors({ origin: "https://your-client-on-render.com" }));
 
 // Connect to MongoDB
