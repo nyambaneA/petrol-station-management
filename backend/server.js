26ed31771902b890c5b7cb0ce0182563643d1aee
NyambaneA <nyambanealfred1539@gmail.com>
2025-03-11 20:34:36 +0300

    added static cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,22 +17,29 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://fueltrack-oigw.onrender.com",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://fueltrack-oigw.onrender.com",
+// ];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         console.log(`Blocked by CORS: ${origin}`);
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true, 
+//   })
+// );
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.log(`Blocked by CORS: ${origin}`);
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, 
+    origin: "*",  // Allow all origins (for testing)
+    credentials: true,
   })
 );
 
